27841a93ee77c5f8a25608fb6d1635ec945fc7c7
lpolon <lpolon@gmail.com>
2020-05-31 22:32:28 -0300

    added util function to parse string to seconds
    1h2m3s returns 3600 + (60 * 2) + 3 + 60
    therefore it can use !play dota time to start audio at the correct time
    
    if no argument is passed, it returns 0

--- src/util.js
+++ src/util.js
@@ -7,4 +7,33 @@ function buildCommands(prefix = '!') {
   }, {});
 }
 
-export { buildCommands };
+// two passes. First one return a object representing the string. i. e.: 1h2m3s -> {h: 1, m: 2, s: 3}
+// second pass returns integer representing input in seconds + 60.
+function parseStringToSeekTime(string) {
+  if (typeof string === 'undefined') return 0
+  let timeString = '';
+  const conversionDic = {
+    h: value => Number(value) * 60 * 60,
+    m: value => Number(value) * 60,
+    s: value => Number(value),
+  };
+  const hashtable = string.split('').reduce((acc, e) => {
+    if (e in conversionDic) {
+      if (timeString === '') return acc;
+      acc[e] = timeString;
+      timeString = '';
+    } else timeString += e;
+    return acc;
+  }, {});
+  // check length, return invalid input
+  const hashtableEntriesArr = Object.entries(hashtable)
+  if (hashtableEntriesArr.length === 0) throw Error(`invalid !play argument input. It must have at least one character value: ${Object.keys(conversionDic).join(', ')}`)
+  return hashtableEntriesArr.reduce((seconds, [key, value]) => {
+    seconds += conversionDic[key](value)
+    return seconds
+  }, 60)
+}
+
+parseStringToSeekTime(undefined) //?
+
+export { buildCommands, parseStringToSeekTime };
